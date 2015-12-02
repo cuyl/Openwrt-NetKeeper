ace3e69be4dbe86a3b0d9bc9a81a1d5ca200c7ce
lycycycycyc <463060544@qq.com>
2015-12-02 21:12:41 +0800

    Update settime.sh

--- src/settime.sh
+++ src/settime.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+uci set network.timesyn=interface
 uci set network.timesyn.proto='pppoe'
 uci set network.timesyn.ifname='eth0.2'
 uci set network.timesyn.username='wuhan'
@@ -10,6 +11,8 @@ ifup timesyn
 uci set firewall.@zone[1].network='wan wan6 NetKeeper timesyn' 
 uci commit firewall
 /etc/init.d/firewall restart
+#江西117.21.211.239
+
 date -u "`curl -sI kd.wuhan.net.cn | awk '/Date:/{gsub(/Jan/,"01",$4);gsub(/Feb/,"02",$4);gsub(/Mar/,"03",$4);gsub(/Apr/,"04",$4);gsub(/May/,"05",$4);gsub(/Jun/,"06",$4);gsub(/Jul/,"07",$4);gsub(/Aug/,"08",$4);gsub(/Sep/,"09",$4);gsub(/Oct/,"10",$4);gsub(/Nov/,"11",$4);gsub(/Dec/,"11",$4);print $5"-"$4"-"$3" "$6;}'`"
 ifdown timesyn
 ifup NetKeeper 
