7f261b69ac4e516535ad9d350abe92be73622f10
lycycycycyc <463060544@qq.com>
2016-01-05 21:48:37 +0800

    更新注释
    更新注释

--- src/settime.sh
+++ src/settime.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-#江西用这个#date -u "`curl -sI 117.21.211.239 | awk '/Date:/{gsub(/Jan/,"01",$4);gsub(/Feb/,"02",$4);gsub(/Mar/,"03",$4);gsub(/Apr/,"04",$4);gsub(/May/,"05",$4);gsub(/Jun/,"06",$4);gsub(/Jul/,"07",$4);gsub(/Aug/,"08",$4);gsub(/Sep/,"09",$4);gsub(/Oct/,"10",$4);gsub(/Nov/,"11",$4);gsub(/Dec/,"11",$4);print $5"-"$4"-"$3" "$6;}'`"
+#date -u "`curl -sI 117.21.211.239 | awk '/Date:/{gsub(/Jan/,"01",$4);gsub(/Feb/,"02",$4);gsub(/Mar/,"03",$4);gsub(/Apr/,"04",$4);gsub(/May/,"05",$4);gsub(/Jun/,"06",$4);gsub(/Jul/,"07",$4);gsub(/Aug/,"08",$4);gsub(/Sep/,"09",$4);gsub(/Oct/,"10",$4);gsub(/Nov/,"11",$4);gsub(/Dec/,"11",$4);print $5"-"$4"-"$3" "$6;}'`"
+#对于非湖北的，改这个↑ip，下面代码全部删掉，上面这行去掉#
 uci set network.timesyn=interface
 uci set network.timesyn.proto='pppoe'
 uci set network.timesyn.ifname='eth0.2'
